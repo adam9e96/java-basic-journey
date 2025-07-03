6e5b3502dda27cd027fb30b10537e4687938c0c0
adam9e96 <adam9e96@gmail.com>
2025-07-03 17:14:55 +0900

    Hello World 콘솔에 출력해보기

---
+++ src/ch01_basics/HelloWorld.java
@@ -0,0 +1,8 @@
+package ch01_basics;
+
+public class HelloWorld {
+    public static void main(String[] args) { // main() 메소드 선언. 프로그램의 시작점이다.
+        System.out.println("Hello World"); // "Hello World" 문자열을 콘솔에 출력한다.
+
+    }
+}
---
+++ src/ch01_basics/readme.md
@@ -0,0 +1,2 @@
+# chapter01
+HelloWorld.java : HelloWorld 콘솔에 출력해보기
