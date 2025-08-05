72af0c9799dcb596d7473577fb1142a0d1c680c2
adam9e96 <adam9e96@gmail.com>
2025-08-06 00:56:17 +0900

    증감, 후위 연산자 사용 예제 추가

---
+++ src/ch03_operators/IncreaseDecreaseOperatorExample.java
@@ -0,0 +1,37 @@
+package ch03_operators;
+
+public class IncreaseDecreaseOperatorExample {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 10;
+        int z;
+
+        x++;
+        ++x;
+        System.out.println("x= " + x);
+        System.out.println("----------------");
+
+        y--;
+        --y;
+        System.out.println("y= " + y);
+        System.out.println("-----------------");
+
+        z = x++;
+        System.out.println("z= " + z);
+        System.out.println("x= " + x);
+        System.out.println("-----------------");
+
+        z = ++x;
+        System.out.println("z= " + z);
+        System.out.println("x= " + x);
+        System.out.println("-----------------");
+
+        // 여기까지 x 는 14, y 는 8임
+        z = ++x + y++;
+        System.out.println("x= " + x); // 15
+        System.out.println("y= " + y); // 9
+        System.out.println("z= " + z); // 23
+
+
+    }
+}
