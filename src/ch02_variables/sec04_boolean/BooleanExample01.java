5afcf4376c9a47d42ea37e7d0af80452408a635b
adam9e96 <adam9e96@gmail.com>
2025-08-06 19:19:24 +0900

    boolean, final 타입 사용법 예제추가

---
+++ src/ch02_variables/sec04_boolean/BooleanExample01.java
@@ -0,0 +1,12 @@
+package ch02_variables.sec04_boolean;
+
+public class BooleanExample01 {
+    public static void main(String[] args) {
+        // 논리형 (Boolean Type)
+        boolean isTrue = true; // true 또는 false만 가능
+        boolean isFalse = false;
+
+        System.out.println(isTrue);
+        System.out.println(isFalse);
+    }
+}
---
+++ src/ch02_variables/sec06_final/FinalExample01.java
@@ -0,0 +1,16 @@
+package ch02_variables.sec06_final;
+
+public class FinalExample01 {
+    public static void main(String[] args) {
+
+        // final 변수는 상수 취급
+        // 상수 취급을 받음 => 초기값 이후로 값 변경 불가능함
+        final int YEAR = 2025;
+        final String MOJANG = "MINECRAFT";
+
+//        YEAR = 2026; // 컴파일 에러
+
+        System.out.println(YEAR);
+        System.out.println(MOJANG);
+    }
+}
