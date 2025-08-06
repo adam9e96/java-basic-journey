5f4ae32656addf7e22ce281588aaa570517b78fc
adam9e96 <adam9e96@gmail.com>
2025-08-07 01:01:53 +0900

    비교 연산자 예제 추가

---
+++ src/ch03_operators/sec04_comparison_operators/ComparisonOperatorExample01.java
@@ -0,0 +1,36 @@
+package ch03_operators.sec04_comparison_operators;
+
+// 비교 연산자
+public class ComparisonOperatorExample01 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        System.out.println("a= " + a + ", b = " + b);
+
+        // 정수 비교
+        System.out.println("a == b: " + (a == b)); // false
+        System.out.println("a != b: " + (a != b)); // true
+        System.out.println("a > b: " + (a > b));  // false
+        System.out.println("a < b: " + (a < b)); // true
+        System.out.println("a >= b: " + (a >= b)); // false
+        System.out.println("a <= b: " + (a <= b)); // true
+
+        // 문자 비교
+        char ch1 = 'A'; // 65
+        char ch2 = 'B'; // 66
+
+        System.out.println("\nch1 = " + ch1 + ", ch2 = " + ch2);
+        System.out.println("ch1 == ch2: " + (ch1 == ch2)); // false
+        System.out.println("ch1 != ch2: " + (ch1 != ch2)); // true
+        System.out.println("ch1 < ch2: " + (ch1 < ch2));   // true
+
+        // 실수 비교
+        double x = 3.14;
+        double y = 2.71;
+
+        System.out.println("\nx = " + x + ", y = " + y);
+        System.out.println("x > y: " + (x > y));   // true
+        System.out.println("x == y: " + (x == y)); // false
+    }
+}
---
+++ src/ch03_operators/sec04_comparison_operators/ComparisonOperatorExample02.java
@@ -0,0 +1,15 @@
+package ch03_operators.sec04_comparison_operators;
+
+public class ComparisonOperatorExample02 {
+    public static void main(String[] args) {
+        // 비교 연산자
+        int d = 10;
+        int e = 5;
+        System.out.println("d는 e보다 크다 " + (d > e));
+        System.out.println("d는 e보다 작다 " + (d < e));
+        System.out.println("d는 e와 같다 " + (d == e));
+        System.out.println("d와 e는 다르다 " + (d != e));
+        System.out.println("d는 e 와 작거나 같다" + (d <= e));
+        System.out.println("d는 e와 크거나 같다" + (d >= e));
+    }
+}
