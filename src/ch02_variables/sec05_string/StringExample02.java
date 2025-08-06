8c4157ee2c631527ad6e3a9cf213321b5ef11d77
adam9e96 <adam9e96@gmail.com>
2025-08-06 19:18:17 +0900

    참조타입 문자열 사용예시 추가

---
+++ src/ch02_variables/sec05_string/StringExample02.java
@@ -0,0 +1,11 @@
+package ch02_variables.sec05_string;
+
+public class StringExample02 {
+    public static void main(String[] args) {
+        // String은 참조 변수(Reference Variable)임
+        // 참조 변수 종류: 객체, 배열, 문자열등이 있음
+        String greetString = "Hello";
+        System.out.println("문자열 길이: "+greetString.length()); // 기본 타입과는 다르게 기본 제공하는 메서드가 있다.
+    }
+
+}
