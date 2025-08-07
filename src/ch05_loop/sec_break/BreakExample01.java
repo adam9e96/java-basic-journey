3f5f05d403a98aac38b8d5523c4707d5227a1bca
adam9e96 <adam9e96@gmail.com>
2025-08-08 01:12:18 +0900

    break 사용법 추가

---
+++ src/ch05_loop/sec_break/BreakExample01.java
@@ -0,0 +1,21 @@
+package ch05_loop.sec_break;
+
+import java.util.Scanner;
+
+public class BreakExample01 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("exit를 입력하면 종료합니다.");
+		while(true) {
+			System.out.print(">>");
+			String text = scanner.nextLine(); // 한 라인 읽기
+			if(text.equals("exit")) { // "exit" 가 입력되면 반복 종료
+				break; // while 문을 벗어남. break은 루프문을 탈출함
+			}
+			
+		}
+		System.out.println("종료합니다...");
+		scanner.close();
+	}
+}
