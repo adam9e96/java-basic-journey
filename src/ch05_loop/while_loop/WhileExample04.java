1ac748538a323cff7fe4cd96bdf25f061dd69210
adam9e96 <adam9e96@gmail.com>
2025-08-07 22:38:02 +0900

    While 예시 문제 추가

---
+++ src/ch05_loop/while_loop/WhileExample04.java
@@ -0,0 +1,30 @@
+package ch05_loop.while_loop;
+
+import java.util.Scanner;
+
+public class WhileExample04 {
+    public static void main(String[] args) {
+        int count = 0; // count는 입력된 정수의 개수
+        int sum = 0; // sum은 합
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("정수를 입력하고 마지막에 -1을 입력하세요.");
+        int n = 0;
+        n = scanner.nextInt(); // 정수를 입력받음
+        while (n != -1) {
+            sum += n; // 입력한 정수를 더함
+            count++;  // 입력한 횟수 카운트
+            n = scanner.nextInt();
+        }
+
+
+        if (count == 0) {
+            System.out.println("입력된 수가 없습니다.");
+        } else {
+            System.out.print("정수의 개수는 " + count + "개이며 ");
+            System.out.println("평균은 " + (double) sum / count + "입니다.");
+        }
+
+        scanner.close();
+    }
+}
