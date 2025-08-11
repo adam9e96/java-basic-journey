313e3d8f8f925209a9ce76c352c7e6ab25cee0ed
adam9e96 <adam9e96@gmail.com>
2025-08-11 22:29:05 +0900

    chat 타입 설명 추가

---
+++ src/ch02_variables/sec01_variable/CharExample.java
@@ -0,0 +1,21 @@
+package ch02_variables.sec01_variable;
+
+public class CharExample {
+    public static void main(String[] args) {
+        char ch1 = 'A';
+        char ch2 = 65;         // 'A'
+        char ch3 = '\uAC00';   // '가'
+
+        System.out.println(ch1); // A
+        System.out.println(ch2); // A
+        System.out.println(ch3); // 가
+
+        // char → int (자동 형변환)
+        int code = ch1;
+        System.out.println(code); // 65
+
+        // int → char (명시적 형변환)
+        char ch4 = (char)97;
+        System.out.println(ch4); // a
+    }
+}
