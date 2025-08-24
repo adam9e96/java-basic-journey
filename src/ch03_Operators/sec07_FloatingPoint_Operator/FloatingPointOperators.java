5a6fc6c9f4f74bdab45e2c850686602be8b12a03
adam9e96 <adam9e96@gmail.com>
2025-08-24 18:20:05 +0900

    ch03 실수

--- src/ch03_Operators/sec07_FloatingPoint_Operator/FloatingPointOperators.java
+++ src/ch03_Operators/sec07_FloatingPoint_Operator/FloatingPointOperators.java
@@ -1,5 +1,6 @@
-package ch03_operators;
+package ch03_Operators.sec07_FloatingPoint_Operator;
 
+// 실수 계산시 주의할점
 public class FloatingPointOperators {
     public static void main(String[] args) {
         int pizza = 1; // 피자 1판
@@ -10,9 +11,9 @@ public class FloatingPointOperators {
         System.out.println("피자 1판에서 남은 양: " + remaining);
         // 피자 1판에서 남은 양: 0.29999999999999993
         /*
-            * 위의 결과가 정확히 0.3이 되지 않는 이유는
-            * 실수 타입인 double이 부동 소수점 방식을 사용하기 때문이다.(float, double 등)
-            * 정확한 계산이 필요하다면 정수 연산으로 변경해서 계산해야 한다.
+         * 위의 결과가 정확히 0.3이 되지 않는 이유는
+         * 실수 타입인 double이 부동 소수점 방식을 사용하기 때문이다.(float, double 등)
+         * 정확한 계산이 필요하다면 정수 연산으로 변경해서 계산해야 한다.
          */
 
         // 정수 연산으로 변경
@@ -26,9 +27,9 @@ public class FloatingPointOperators {
         System.out.println("피자 1판에서 남은 양: " + remainingSlices / 10.0);
         // 피자 1판에서 남은 양: 0.3
         /*
-            * 위의 결과는 정확히 0.3이 된다.
-            * 정수 연산을 통해 정확한 결과를 얻을 수 있다.
-            * 실수 연산을 할 때는 항상 주의해야 한다.
+         * 위의 결과는 정확히 0.3이 된다.
+         * 정수 연산을 통해 정확한 결과를 얻을 수 있다.
+         * 실수 연산을 할 때는 항상 주의해야 한다.
          */
     }
 }
